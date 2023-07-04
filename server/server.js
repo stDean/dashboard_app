6aa31a64acd3bdd4d05e7c3fe55fc9d84463f11b
Toba-dean <tobaogundimu@gmail.com>
2023-07-04 21:15:31 +0100

    added connect to db logic

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
-console.log('Hello Node');
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+
+import connectDB from "./connect/connectDB";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
