6a72d5cae3ac655a06483fb9f50f525ec7f3e1b5
Toba-dean <tobaogundimu@gmail.com>
2023-07-07 00:32:47 +0100

    added the navbar component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as FlexBetween } from "./FlexBetween";
+export { default as NavBar } from "./NavBar";
--- client/src/container/layout/index.jsx
+++ client/src/container/layout/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { UseSelector } from "react-redux";
+
+import { NavBar } from "components";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <Box width="100%" height="100%">
+      <Box>
+        <NavBar />
+        {/* outlets basically nested routes component */}
+        <Outlet />
+      </Box>
+    </Box>
   )
-}
+};
 
-export default Layout
+export default Layout;
